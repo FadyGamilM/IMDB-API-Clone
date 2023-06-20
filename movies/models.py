84a6c966d9e4965615138701bd3bdda06dcb3d00
[FadyGamilM] <1700961@eng.asu.edu.eg>
2023-06-20 19:08:43 +0300

    FEATURE | Movies_App | implement first model (Movie) model

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Movie(models.Model):
+    name = models.CharField(blank=False, max_length=200)
+    is_published = models.BooleanField(default=True)
+    description = models.CharField(max_length=500)
+
+    def __str__(self) -> str:
+        return self.name
