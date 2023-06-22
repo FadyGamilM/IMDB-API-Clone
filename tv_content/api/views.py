42c67dbfb49a79526f67eff6ac87b309e925449e
FadyGamilM <1700961@eng.asu.edu.eg>
2023-06-22 13:17:44 +0300

    FEATURE | Content_App | Finish the CRUD view for StreamingPlatform resource

--- tv_content/api/views.py
+++ tv_content/api/views.py
@@ -68,3 +68,71 @@ class ContentDetailsActionsApiView(APIView):
             return Response({'error': 'no resource found with given priamry key'}, status=status.HTTP_404_NOT_FOUND)
         content.delete()
         return Response(data = "deleted", status= status.HTTP_204_NO_CONTENT)
+    
+
+
+
+
+class StreamingPlatformListApiView(APIView):
+    def get(self, request: Request):
+        # 1. we need find all streaming platforms instances
+        # 2. serialize them 
+        # 3. return the response with proper status code 
+        resources = StreamingPlatform.objects.all()
+        serializer = StreamingPlatformSerializer(resources, many=True)
+        return Response(data=serializer.data, status=status.HTTP_200_OK)
+    
+
+class StreamingPlatformCreateApiView(APIView):
+    def post(self, request:Request):
+        # 1. first we need to serialize the data from the request
+        # 2. check if the data is serialized and valid 
+        # 3. persist the created instance by calling .save()
+        # 4. return the proper response 
+        serilizer = StreamingPlatformSerializer(request.data)
+        if serilizer.is_valid():
+            serilizer.save()
+            return Response(serilizer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serilizer.errors(), status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
+class StreamingPlatformDetailsActionsApiView(APIView):
+    def get(self, request:Request, pk:int):
+        # 1. try to found the resource with given pk 
+        # 2. serialize the found resource 
+        # 3. return the response 
+        try:
+            resource = StreamingPlatform.objects.get(pk = pk)
+        except Content.DoesNotExist:
+            return Response({'error': 'no resource found with given priamry key'}, status=status.HTTP_404_NOT_FOUND)
+        serialized_data = StreamingPlatformSerializer(resource)
+        return Response(serialized_data, status=status.HTTP_200_OK)
+    
+    def put(self, request:Request, pk:int):
+        # 1. try to found the resource with given pk 
+        # 2. serialize the found resource 
+        # 3. update the data by calling save 
+        # 4. return the proper response 
+        try:
+            resource = StreamingPlatform.objects.get(pk = pk)
+        except Content.DoesNotExist:
+            return Response({'error': 'no resource found with given priamry key'}, status=status.HTTP_404_NOT_FOUND)
+        serializer = StreamingPlatformSerializer(instance=resource, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status= status.HTTP_202_ACCEPTED)
+        else:
+            return Response({"error": "couldn't update the resource"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+    def delete(self, request:Request, pk:int):
+        # 1. try to found the resource with given pk 
+        # 2. serialize the found resource 
+        # 3. delete the data by calling delete  
+        # 4. return the proper response 
+        try:
+            resource = StreamingPlatform.objects.get(pk = pk)
+        except StreamingPlatform.DoesNotExist:
+            return Response({'error': 'no resource found with given priamry key'}, status=status.HTTP_404_NOT_FOUND)
+        resource.delete()
+        return Response(data = "deleted", status= status.HTTP_204_NO_CONTENT)
